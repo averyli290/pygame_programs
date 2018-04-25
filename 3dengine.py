9e4a4e15063e1c753f7f5b7846c01f2e82b3326f
averyli290 <averyli408@gmail.com>
2018-04-24 17:59:15 -0700

    Updating the program

--- 3dengine.py
+++ 3dengine.py
@@ -1,4 +1,4 @@
-import pygame, sys, math
+import pygame, sys, math, random
 
 # FROM YOUTUBE VIDEO https://www.youtube.com/watch?v=g4E9iq0BixA
 # This was learned, not just copied
@@ -17,16 +17,17 @@ def main():
     pygame.event.set_grab(1)
 
     class Player(pygame.sprite.Sprite):
-        def __init__(self, screen, color, pos=(0, 0, 0), rot=(0, 0)):
+        def __init__(self, screen, color, pos=(0, 0, 0), rot=(0, 0), verts=[], edges=[], faces=[]):
             pygame.sprite.Sprite.__init__(self)
 
             self.color = color
             self.screen = screen
 
-            # List of verticies and edges for the map
-            self.verts = []
-            self.edges = []
-            
+            # List of faces, verticies and edges for the map
+            self.verts = verts
+            self.edges = edges
+            self.faces = faces
+
             # The variable that is divided by z for simulating depth (The width of the screen divided by 2 because that is the center)
             self.depthVar = self.screen.get_width() 
             # Center of screen
@@ -37,35 +38,35 @@ def main():
             self.campos = list(pos)
             self.camrot = list(rot)
             self.clock = pygame.time.Clock()
-
-            self.mousesens = 10
-            self.radian = 0
+            
+            # The higher this variable is, the lower the sensitivity (because we are dividing values by this number to get the rotation)
+            self.mousesens = 10000
 
         def events(self, event):
-            return 1
             if event.type == pygame.MOUSEMOTION:
                 x, y = event.rel
                 x /= self.mousesens
                 y /= self.mousesens
 
                 self.camrot[0] += y
-                self.camrot[1] += x
+                self.camrot[1] += x 
 
         def update(self, dt, key):
             
-            # Adding movements
+            # Adding movements (s is the distance to move)
             s = dt*10
 
             if key[pygame.K_q]: self.campos[1] += s
             if key[pygame.K_e]: self.campos[1] -= s
             
-            #x, y = s*math.sin(self.camrot[1]), s*math.cos(self.camrot[1])
-
-            if key[pygame.K_w]: self.campos[2] += s
-            if key[pygame.K_a]: self.campos[0] -= s
-            if key[pygame.K_s]: self.campos[2] -= s
-            if key[pygame.K_d]: self.campos[0] += s
+            x, y = s*math.sin(self.camrot[1]), s*math.cos(self.camrot[1])
             
+            # Applying the respective movements
+            if key[pygame.K_w]: self.campos[0] += x; self.campos[2] += y
+            if key[pygame.K_a]: self.campos[0] -= y; self.campos[2] += x
+            if key[pygame.K_s]: self.campos[0] -= x; self.campos[2] -= y
+            if key[pygame.K_d]: self.campos[0] += y; self.campos[2] -= x
+
         def handle_keys(self):
             dt = self.clock.tick()/1000
 
@@ -80,14 +81,17 @@ def main():
 
                 self.events(event)
 
+            # Rendering the faces
+            #self.renderFaces()
+            self.renderEdges()
             # Updating camera view depending on key inputs (in small increments, hence the 1000)
-            self.radian += dt
             key = pygame.key.get_pressed()
             self.update(dt, key)
 
         def initializeSetting(self):
             self.verts = [(-1, -1, -1), (1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1)]
             self.edges = [(0, 1), (1, 2), (2, 3), (3, 0), (4, 5), (5, 6), (6, 7), (7, 4), (0, 4), (1, 5), (2, 6), (3, 7)] 
+            self.faces = [(0, 1, 2, 3), (4, 5, 6, 7), (0, 1, 5 , 4), (2, 3, 7 ,6), (0, 3, 7, 4), (1, 2, 6, 5)]
 
         def rotate2d(self, pos, rad):
             # Based on unit circle (counterclockwise)
@@ -103,6 +107,25 @@ def main():
 
             # Adding in the rotation
             return x*c-y*s, y*c+x*s
+        
+        def convertCoords(self, x, y, z, perspective=True):
+            # If perspective, applying the depth to x and y
+
+            # This converts the coords from 3d to 2d for pygame
+            x -= self.campos[0]
+            y -= self.campos[1]
+            z -= self.campos[2]
+            
+            # Rotating on the y axis first, then x axis
+            x, z = self.rotate2d((x, z), self.camrot[1])
+            y, z = self.rotate2d((y, z), self.camrot[0])
+            
+            # Simulating depth (The larger z is, the further away it will appear)
+            depth = self.depthVar/z
+            if perspective: x, y = x*depth, y*depth
+            
+            return x, y, z, depth
+
 
         def renderPoints(self):
             
@@ -111,23 +134,14 @@ def main():
             
             # Rendering verticies
             for x,y,z in self.verts:
+                
+                # Converting coords
+                x,y,z,depth = self.convertCoords(x, y, z)
 
-                x -= self.campos[0]
-                y -= self.campos[1]
-                z -= self.campos[2]
-
-                # Adding in the mouse rotation
-                # Rotating on the y axis first, then x axis
-                x, z = self.rotate2d((x, z), self.camrot[1])
-                y, z = self.rotate2d((y, z), self.camrot[0])
-
-
-                # Simulating depth (The larger z is, the further away it will appear)
-                depth = self.depthVar/z
-                x, y = x*depth, y*depth
-
-                # Creating the verticies (self.cx and self.cy because that is the center)
-                pygame.draw.circle(self.screen, (0, 0, 0), (self.cx+int(x), self.cy+int(y)), 6)
+                # Creating the verticies (self.cx and self.cy because that is the center) 
+                # Making the radius of the verticies
+                radius = abs(int(20/z))
+                pygame.draw.circle(self.screen, (0, 0, 0), (self.cx+int(x), self.cy+int(y)), radius) 
 
             pygame.display.flip()
         
@@ -140,38 +154,70 @@ def main():
             for edge in self.edges:
                 
                 # Putting the edge points into a list
-                points = []
+                screen_coords= []
                 
                 for x,y,z in (self.verts[edge[0]], self.verts[edge[1]]):
 
-                    x -= self.campos[0]
-                    y -= self.campos[1]
-                    z -= self.campos[2]
-
-                    # Adding in the mouse rotation
-                    # Rotating on the y axis first, then x axis
-
-                    x, z = self.rotate2d((x, z), self.radian)
-
-                    x, z = self.rotate2d((x, z), self.camrot[1])
-                    y, z = self.rotate2d((y, z), self.camrot[0])
-
-                    # Getting the x and y coords for both points
-                    depth = self.depthVar/z
-                    x, y = x*depth, y*depth
-
+                    # Converting coords
+                    x,y,z,depth = self.convertCoords(x, y, z)
+                    
                     # Adding the point to the list
-                    points += [(self.cx+int(x), self.cy+int(y))]
+                    screen_coords += [(self.cx+int(x), self.cy+int(y))]
 
-                pygame.draw.line(self.screen, (0, 0, 0), points[0], points[1], 1)
+                pygame.draw.line(self.screen, (0, 0, 0), screen_coords[0], screen_coords[1], 1)
 
             pygame.display.flip()
+
+        def renderFaces(self):
+            # TO BE FIXED 
+
+            # Clearing the screen
+            screen.fill((255, 255, 255))
+            
+            # Translating all verticies to start, points is for 3d, screen_coords for 2d
+            screen_coords = []
+            points = []
+            for x,y,z in self.verts:
+                x, z = self.rotate2d((x, z), self.camrot[1])
+                x, y = self.rotate2d((x, y), self.camrot[0])
+
+                points += [(x,y,z)]
+                
+                depth = self.depthVar/z
+                x,y = x*depth, y*depth
+                
+                screen_coords += [(self.cx+int(x), self.cy+int(y))]
+
+
+            # Rendering faces
+            face_list = []
+
+            for face in self.faces:
+
+                # Making sure that the face is in front of the camera
+                on_screen = False
+                
+                # For each of the verticies in the face, if the z coord is > 0, then true
+                for i in face:
+                    if points[i][2] > 0: on_screen = True; break
+                
+                if on_screen:
+                    # Getting the coordinates for each vertex on the face
+                    coords = [screen_coords[i] for i in face]
+
+                    # Putting the coords into the list for drawing later
+                    face_list += [coords]
+            
+            for i in range(len(face_list)):
+                color = random.randint(0, 255)
+                pygame.draw.polygon(self.screen, (color, color, color), face_list[i], 0)
 
     p = Player(screen, (0, 255, 255), (0, 0, -5))
     p.initializeSetting()
 
     while True:
         p.handle_keys()
-        p.renderEdges()
+        #p.renderEdges()
+        #p.renderPoints()
 
 main()
